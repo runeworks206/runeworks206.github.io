b6254e784773dda7c816e4c27fe0d2d78b299c33
runeworks206 <122604887+runeworks206@users.noreply.github.com>
2023-07-30 03:40:59 +1000

    Update rw-utility.js

--- rw-utility/rw-utility.js
+++ rw-utility/rw-utility.js
@@ -53,6 +53,25 @@ runeworks.utility = (function() {
   /* Retrieve the key that maps to a provided value */
   let key = function(obj, v) { for (var prop in obj) { if (obj.hasOwnProperty(prop)) { if (obj[prop] === v) { return prop } } } }
 
+  /* Normal distribution sampling */
+  // https://stackoverflow.com/a/49434653
+  let nrandom = function(min, max, skew) {
+    let u = 0, v = 0;
+    while(u === 0) u = Math.random() //Converting [0,1) to (0,1)
+    while(v === 0) v = Math.random()
+    let num = Math.sqrt( -2.0 * Math.log( u ) ) * Math.cos( 2.0 * Math.PI * v )
+  
+    num = num / 10.0 + 0.5 // Translate to 0 -> 1
+    if (num > 1 || num < 0) 
+      num = randn_bm(min, max, skew) // resample between 0 and 1 if out of range
+  
+    else {
+      num = Math.pow(num, skew) // Skew
+      num *= max - min // Stretch to fill range
+      num += min // offset to min
+    }
+    return num }
+  
   /* Rounds numbers */
   let round = function(num, scale) {
       if (!('' + num).includes('e')) { 
@@ -156,6 +175,7 @@ runeworks.utility = (function() {
     comma         : commaThis,
     interval      : interval,
     key           : key,
+    nrandom       : nrandom,
     round         : round,
     uniqueArray   : uniqueArray,
     uuid          : uuid,
